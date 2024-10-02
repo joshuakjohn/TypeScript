6be70e62ddc69492424b4de84210bc4836b03632
Joshua K John <joshuakjohn7@gmail.com>
2024-10-02 22:27:32 +0530

    [joshua] add - added example for enum in ts

---
+++ EnumPractice/examples.js
@@ -0,0 +1,65 @@
+"use strict";
+/*
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+  }
+  
+  let move: Direction = Direction.Up;
+  
+  console.log(move);  // Output: 0 (default value for the first enum member)
+  */
+/*
+ enum Status {
+   Success = 200,
+   NotFound = 404,
+   ServerError = 500
+ }
+ 
+ let response: Status = Status.NotFound;
+ 
+ console.log(response);  // Output: 404
+*/
+/*
+enum Colors {
+  Red = "RED",
+  Green = "GREEN",
+  Blue = "BLUE"
+}
+
+let color: Colors = Colors.Green;
+
+console.log(color);  // Output: "GREEN"
+*/
+/*
+enum Size {
+  Small = 1,
+  Medium,
+  Large
+}
+
+let sizeName: string = Size[2];
+
+console.log(sizeName);  // Output: "Medium"
+*/
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["User"] = 1] = "User";
+    Role[Role["Guest"] = 2] = "Guest";
+})(Role || (Role = {}));
+function checkAccess(role) {
+    if (role === Role.Admin) {
+        console.log("Access granted: Admin");
+    }
+    else if (role === Role.User) {
+        console.log("Access granted: User");
+    }
+    else {
+        console.log("Access denied");
+    }
+}
+checkAccess(Role.Admin); // Output: Access granted: Admin
+checkAccess(Role.Guest); // Output: Access denied
