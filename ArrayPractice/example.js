ed34fd58a8b9b02aefbebc4a5de7f748a1850fc5
Joshua K John <joshuakjohn7@gmail.com>
2024-10-02 21:55:24 +0530

    [joshua] add - added example for array in ts

---
+++ ArrayPractice/example.js
@@ -0,0 +1,7 @@
+"use strict";
+//String array
+const arr = ["hello", "world"];
+console.log(arr);
+//Number array
+const num = [1, 2, 3, 4];
+console.log(num);
---
+++ ArrayPractice/example.ts
@@ -0,0 +1,7 @@
+//String array
+const arr: string[] = ["hello", "world"];
+console.log(arr);
+
+//Number array
+const num: Number[] = [1,2,3,4];
+console.log(num);
