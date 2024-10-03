92ea002b1104daee9540e3da2b70fca889e010f6
Joshua K John <joshuakjohn7@gmail.com>
2024-10-03 13:11:23 +0530

    [joshua] add - added class examples in ts

---
+++ ClassPractice/class.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+class Person {
+    name: string;
+  }
+  
+  const person = new Person();
+  person.name = "Jane";
+  */
+/*
+class Person {
+ private name: string;
+
+ public constructor(name: string) {
+   this.name = name;
+ }
+
+ public getName(): string {
+   return this.name;
+ }
+}
+
+const person = new Person("Jane");
+console.log(person.getName()); // person.name isn't accessible from outside the class since it's private
+*/
+class Person {
+    constructor(name) {
+        // name cannot be changed after this initial definition, which has to be either at it's declaration or in the constructor.
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+const person = new Person("Jane");
+console.log(person.getName());
